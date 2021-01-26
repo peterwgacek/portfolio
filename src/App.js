38a0de2b811d2a3b8886e8f779acd6e2a193e8f2
Peter Gacek <peterwgacek@gmail.com>
2021-01-26 18:24:00 -0500

    Adds Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Portfolio Project</h1>
     </div>
   );
