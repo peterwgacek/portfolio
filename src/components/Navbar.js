0b3d0717912e248e436b29949c29e9426d8bff44
Peter Gacek <peterwgacek@gmail.com>
2021-01-27 15:23:28 -0500

    Completes Navbar for now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import logo from "../logo.png";
+import logo from '../logo.png';
+
 
 const Navbar = () => {
     return (
         <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-            <div className="container">
-                <a className='navbar-brand' href='#'><img className="logo" src={logo} alt="logo image" /></a>
+            <div className='container'>
+                <a className='navbar-brand' href='#'><img className='logo' src={logo} alt='logo' /></a>
 
                 <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-                    <span className='navbar-toggler-icon'></span>
+                    <i class="fas fa-bars"></i>
                 </button>
 
                 <div className='collapse navbar-collapse' id='navbarSupportedContent'>
