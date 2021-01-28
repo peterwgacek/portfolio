7465a580070b55ad294e0bc3addcb984e902a243
Peter Gacek <peterwgacek@gmail.com>
2021-01-28 10:59:58 -0500

    Finishes Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import logo from '../logo.png';
 
 
 const Header = () => {
     return (
         <div className="header-wrapper">
             <div className="main-info">
-                <img className='logo' src={logo} alt='logo' />
                 <h1>Peter W Gacek</h1>
                 <h2>Project Manager turned Software Engineer</h2>
+                <a href="#" className='btn-main-offer'>Contact Me</a>
             </div>
         </div >
     )
