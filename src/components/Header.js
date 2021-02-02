f259679de420596c1fd19311ca1ec01f6e9aa4c3
Peter Gacek <peterwgacek@gmail.com>
2021-02-02 12:00:30 -0500

    Connects Header Contact Me to component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-scroll';
 
 const Header = () => {
     return (
@@ -8,7 +8,7 @@ const Header = () => {
                 <h1>Peter W Gacek</h1>
                 <h2>Project Manager turned Software Engineer</h2>
                 <div className="links">
-                    <a href='#' className='btn-main-offer'>Contact Me</a>
+                    <Link smooth={true} to="contact" href='#' className='btn-main-offer'>Contact Me</Link>
                     <a className='resume' href='https://docs.google.com/document/d/19VLOE1uNlk19l53EkbLxcWVm6NW-Hul3UAtB-rhu00U/edit?usp=sharing' target='_blank' rel='noreferrer' >Resume</a>
                 </div>
             </div>
