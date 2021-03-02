b9f8488101f23048d618bc743caab27befadee6e
Peter Gacek <peterwgacek@gmail.com>
2021-03-02 11:15:27 -0500

    Commit before moving into componnet subfolders

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import logo from '../assets/logo.png';
 import { Link } from 'react-scroll';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
